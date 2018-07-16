3c96ea31bf9df051194f8039ceb643383e465b73
SunPv5 <30717313+SunPv5@users.noreply.github.com>
2018-07-16 15:18:45 +0800

    判断是否有Root权限

---
+++ GetRootPerssion.java
@@ -0,0 +1,42 @@
+package com.example.sp.androiddevelopmentutils.utils;
+
+import java.io.PrintWriter;
+
+/**
+ * Created by SP on 2018/6/7.
+ * 判断是否有root权限工具类
+ */
+
+public class GetRootPerssion {
+
+    public static boolean hasRootPerssion(){
+        PrintWriter PrintWriter = null;
+        Process process = null;
+        try {
+            process = Runtime.getRuntime().exec("su");
+            PrintWriter = new PrintWriter(process.getOutputStream());
+            PrintWriter.flush();
+            PrintWriter.close();
+            int value = process.waitFor();
+            return returnResult(value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            if(process!=null){
+                process.destroy();
+            }
+        }
+        return false;
+    }
+
+    private static boolean returnResult(int value){
+        // 代表成功
+        if (value == 0) {
+            return true;
+        } else if (value == 1) { // 失败
+            return false;
+        } else { // 未知情况
+            return false;
+        }
+    }
+}
