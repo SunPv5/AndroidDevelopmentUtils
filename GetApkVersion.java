c7e828dc91ce79d02fe0b357a645d61ba1b02a24
SunPv5 <30717313+SunPv5@users.noreply.github.com>
2018-07-16 15:14:18 +0800

    获取apk版本号

---
+++ GetApkVersion.java
@@ -0,0 +1,47 @@
+package com.example.sp.androiddevelopmentutils.utils;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+/**
+ * Created by sp on 2018/6/1
+ * 获取应用版本号
+ */
+
+public class GetApkVersion {
+
+    /**
+     * 获取当前本地apk的版本
+     *
+     * @param mContext
+     * @return
+     */
+    public static int getVersionCode(Context mContext) {
+        int versionCode = 0;
+        try {
+            //获取软件版本号，对应AndroidManifest.xml下android:versionCode
+            versionCode = mContext.getPackageManager().
+                    getPackageInfo(mContext.getPackageName(), 0).versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return versionCode;
+    }
+
+    /**
+     * 获取版本号名称
+     *
+     * @param context 上下文
+     * @return
+     */
+    public static String getVersionName(Context context) {
+        String verName = "";
+        try {
+            verName = context.getPackageManager().
+                    getPackageInfo(context.getPackageName(), 0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return verName;
+    }
+}
